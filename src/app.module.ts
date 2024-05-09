11ed6df43a4ac3487e549d388ccb87c68a2c411d
Azutech <azunna.onugha@gmail.com>
2024-05-09 12:02:21 +0100

    chore () : Setup Database and Server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // Import ConfigModule with default options
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule], // Import ConfigModule to use ConfigService
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'), // Get MongoDB URI from environment variables
+      }),
+      inject: [ConfigService], // Inject ConfigService into the factory function
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<string>('PORT');
+  app.setGlobalPrefix('api/v1');
+
+
+  await app.listen(port, () => logger.log(`App running on Port: ${port}`));
 }
-bootstrap();
+
+bootstrap()
